7490c347c411d947cc98800f8f600d6328f3fe1f
andormade <hngrhorace@gmail.com>
2017-07-28 16:51:05 +0200

    added canvas drawing + color replacing

--- src/index.js
+++ src/index.js
@@ -11,11 +11,28 @@ function getChannelCount(canvas) {
 
 function bytePosition2Coordinates(canvas, pos) {
 	let byteWidth = canvas.width * getChannelCount(canvas);
-	return [Math.floor(pos / byteWidth), pos % byteWidth / getChannelCount(canvas)];
+	return [
+		Math.floor(pos / byteWidth),
+		pos % byteWidth / getChannelCount(canvas)
+	];
 }
 
 function coordinates2bytePosition(canvas, x, y) {
 	return y * canvas.width * getChannelCount(canvas) + x;
+}
+
+function forEachPixel(canvas, callback) {
+	for (let bytePos = 0; bytePos < canvas.data.length; bytePos += getChannelCount(canvas)) {
+		let [x, y] = bytePosition2Coordinates(canvas, bytePos);
+		callback(x, y, bytePos);
+	}
+}
+
+function forEachByte(canvas, callback) {
+	for (let bytePos = 0; bytePos < canvas.data.length; bytePos++) {
+		let [x, y] = bytePosition2Coordinates(canvas, bytePos);
+		callback(x, y, bytePos);
+	}
 }
 
 export function createCanvas(width, height, hasAlphaChannel = true) {
@@ -42,13 +59,13 @@ export function cloneCanvas(canvas) {
 }
 
 export function drawPixel(canvas, x, y, color) {
-	let [red, green, blue, alpha] = color;
+	let [red, green, blue, alpha] = color,
+		newCanvas = cloneCanvas(canvas),
+		bytePos = coordinates2bytePosition(canvas, x, y);
 
-	let newCanvas = cloneCanvas(canvas);
-
-	newCanvas.data[coordinates2bytePosition(canvas, x, y) + CHANNEL_RED] = red;
-	newCanvas.data[coordinates2bytePosition(canvas, x, y) + CHANNEL_GREEN] = green;
-	newCanvas.data[coordinates2bytePosition(canvas, x, y) + CHANNEL_BLUE] = blue;
+	newCanvas.data[bytePos + CHANNEL_RED] = red;
+	newCanvas.data[bytePos + CHANNEL_GREEN] = green;
+	newCanvas.data[bytePos + CHANNEL_BLUE] = blue;
 
 	return newCanvas;
 }
@@ -60,15 +77,70 @@ export function drawRect(canvas, x, y, width, height, color) {
 
 	for (let i = x; i < x + width; i++) {
 		for (let j = y; j < y + height; j++) {
-			newCanvas.data[coordinates2bytePosition(canvas, i, j) + CHANNEL_RED] = red;
-			newCanvas.data[coordinates2bytePosition(canvas, i, j) + CHANNEL_GREEN] = green;
-			newCanvas.data[coordinates2bytePosition(canvas, i, j) + CHANNEL_BLUE] = blue;
+			let bytePos = coordinates2bytePosition(canvas, i, j);
+			newCanvas.data[bytePos + CHANNEL_RED] = red;
+			newCanvas.data[bytePos + CHANNEL_GREEN] = green;
+			newCanvas.data[bytePos + CHANNEL_BLUE] = blue;
  		}
 	}
 
 	return newCanvas;
 }
 
+export function drawCanvas(destination, source, offsetX, offsetY) {
+	let newCanvas = cloneCanvas(destination);
+
+	forEachPixel(source, (x, y, bytePos) => {
+		if (typeof newCanvas.data[destBytePos] === 'undefined') {
+			return;
+		}
+
+		let destBytePos = coordinates2bytePosition(x + offsetX, y + offsetY);
+
+		newCanvas.data[destBytePos + CHANNEL_RED] = source.data[bytePos + CHANNEL_RED];
+		newCanvas.data[destBytePos + CHANNEL_GREEN] = source.data[bytePos + CHANNEL_GREEN];
+		newCanvas.data[destBytePos + CHANNEL_BLUE] = source.data[bytePos + CHANNEL_BLUE];
+
+		if (source.hasAlphaChannel && destination.hasAlphaChannel) {
+			newCanvas.data[destBytePos + CHANNEL_ALPHA] = source.data[bytePos + CHANNEL_ALPHA];
+		}
+		else if (!source.hasAlphaChannel && destination.hasAlphaChannel) {
+			newCanvas.data[destBytePos + CHANNEL_ALPHA] = 0xff;
+		}
+	});
+
+	return newCanvas;
+}
+
+export function replaceColor(canvas, replacee, replacer) {
+	let newCanvas = cloneCanvas(destination);
+
+	forEachPixel(newCanvas, (x, y, bytePos) => {
+		if (
+			newCanvas.data[bytePos + CHANNEL_RED] === replacee[CHANNEL_RED] &&
+			newCanvas.data[bytePos + CHANNEL_GREEN] === replacee[CHANNEL_GREEN] &&
+			newCanvas.data[bytePos + CHANNEL_BLUE] === replacee[CHANNEL_BLUE]
+		) {
+			newCanvas.data[bytePos + CHANNEL_RED] = replacer[CHANNEL_RED];
+			newCanvas.data[bytePos + CHANNEL_GREEN] = replacer[CHANNEL_GREEN];
+			newCanvas.data[bytePos + CHANNEL_BLUE] = replacer[CHANNEL_BLUE];
+		}
+	});
+
+	return newCanvas;
+}
+
 export function getColor(canvas, x, y) {
-	return canvas[coordinates2bytePosition(canvas, x, y)];
+	let bytePos = coordinates2bytePosition(canvas, x, y),
+		color = [
+			canvas.data[bytePos + CHANNEL_RED],
+			canvas.data[bytePos + CHANNEL_GREEN],
+			canvas.data[bytePos + CHANNEL_BLUE]
+		];
+
+	if (canvas.hasAlphaChannel) {
+		color[CHANNEL_ALPHA] = canvas.data[bytePos + CHANNEL_ALPHA];
+	}
+
+	return color;
 }
